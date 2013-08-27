ae89f2a0b7c48d154ebc13649e3186ae635f6585
Paul Barton <pablo.barton@gmail.com>
2013-08-26 20:44:20 -0400

    Adding a bunch of new stuff before heading home

--- ligfit.py
+++ ligfit.py
@@ -19,9 +19,7 @@ Options:
 """
 
 from docopt import docopt  # Used for command-line argument parsing
-import scipy  # Used for fitting algorithms
-import mpmath
-from mpmath import mp, mpf  # Used for arbitrary precision floats
+from scipy.optimize import curve_fit
 import numpy  # Used for arrays
 import json  # Used to store/retrieve input information
 import matplotlib.pyplot as plt
@@ -44,11 +42,11 @@ def parse_input_file(file_path):
         total_ligand, y_obs, y_err, weight = [], [], [], []
         for data_line in input_lines[4:]:
             vals = data_line.split(' ')
-            total_ligand.append(mpf(vals[0]))
-            y_obs.append(mpf(vals[1]))
-            y_err.append(mpf(vals[2]))
-            weight.append(mpf(vals[3]))
-    data = [numpy.array(i) for i in [total_ligand, y_obs, y_err, weight]]
+            total_ligand.append(vals[0])
+            y_obs.append(vals[1])
+            y_err.append(vals[2])
+            weight.append(vals[3])
+    data = [numpy.array(i, numpy.float64) for i in [total_ligand, y_obs, y_err, weight]]
     return fitting_params, data
 
 
@@ -81,13 +79,10 @@ def calc_abc(kd, alpha, p_total, l_total):
     '''
     Calculates the cubic polynomial constants a, b, and c from the given Kd,
     alpha, p_total, and l_total values.
-
-    This version formulated to work with mpmath for arbitrary floating point
-    precision and numpy arrays.
     '''
-    a = mpf('2') * kd / alpha + mpf('2') * l_total - p_total
-    b = (kd + mpf('2') * l_total - mpf('2') * p_total) * (kd / alpha)
-    c = (mpf('-1') * numpy.power(kd, mpf('2')) * p_total) / alpha
+    a = (2.0 * kd) / alpha + (2.0 * l_total) - p_total
+    b = (kd + 2.0 * l_total - 2.0 * p_total) * (kd / alpha)
+    c = (-1.0 * numpy.power(kd, 2.0) * p_total) / alpha
     return a, b, c
 
 
@@ -96,12 +91,9 @@ def calc_qr(a, b, c):
     Calculates Q and R (functions of a, b, and c) for the cubic solution. The
     value of Q^3 + R^2 will determine if the solution needs to be calculated in
     cartesian or polar coordinates.
-
-    This version formulated to work with mpmath for arbitrary floating point
-    precision and numpy arrays.
     '''
-    q = (mpf('3') * b - numpy.power(a, mpf('2'))) / mpf('9')
-    r = (mpf('9') * a * b - mpf('27') * c - mpf('2') * numpy.power(a, mpf('3'))) / mpf('54')
+    q = (3.0 * b - numpy.power(a, 2.0)) / 9.0
+    r = (9.0 * a * b - 27.0 * c - 2.0 * numpy.power(a, 3.0)) / 54.0
     return q, r
 
 
@@ -109,14 +101,11 @@ def cartesian_cubic(a, q, r):
     '''
     The cartesian coordinate solution of the cubic function; for use when
     Q^3+R^2 > 0
-
-    This version formulated to work with mpmath for arbitrary floating point
-    precision and numpy arrays.
     '''
     #Calculate the three terms of the cubic
-    first = a / mpf('-3')
-    second = numpy.power(r + numpy.power(numpy.power(q, mpf('3')) + numpy.power(r, mpf('2')), mpf('0.5')), mpf('1/3'))
-    third = numpy.power(r - numpy.power(numpy.power(q, mpf('3')) + numpy.power(r, mpf('2')), mpf('0.5')), mpf('1/3'))
+    first = a / -3.0
+    second = numpy.power(r + numpy.sqrt(numpy.power(q, 3) + numpy.power(r, 2)), 1.0 / 3.0)
+    third = numpy.power(r - numpy.sqrt(numpy.power(q, 3) + numpy.power(r, 2)), 1.0 / 3.0)
     return first + second + third
 
 
@@ -124,36 +113,28 @@ def polar_cubic(a, q, r):
     '''
     The polar coordinate solution of the cubic function; for use when
     Q^3+R^2 < 0
-
-    This version formulated to work with mpmath for arbitrary floating point
-    precision and numpy arrays.
     '''
-    theta = mpmath.acos(r / numpy.power(mpf('-1') * numpy.power(q, mpf('3')), mpf('0.5')))
-    return mpmath.cos(theta / mpf('3')) * numpy.power(mpf('-1') * q, mpf('0.5')) * mpf('2') - a / mpf('3')
+    theta = numpy.arccos(r / numpy.sqrt(-1.0 * numpy.power(q, 3.0)))
+    val = numpy.cos(theta / 3.0) * numpy.sqrt(-1.0 * q) * 2.0 - a / 3.0
+    return val
 
 
 def get_pl(kd, alpha, l_total, p):
     '''
     After solving for [P]-free, this function will return the concentration of
     monomer protein bound to ligand: [PL].
-
-    This version formulated to work with mpmath for arbitrary floating point
-    precision and numpy arrays.
     '''
-    numerator = mpf('2') * kd * l_total * p
-    denominator = numpy.power(kd, mpf('2')) + mpf('2') * kd * p + alpha * numpy.power(p, mpf('2'))
+    numerator = 2.0 * kd * l_total * p
+    denominator = numpy.power(kd, 2.0) + 2.0 * kd * p + alpha * numpy.power(p, 2.0)
     return numerator / denominator
 
 def get_plp(kd, alpha, l_total, p):
     '''
     After solving for [P]-free, this function will return the concentration of
     dimer protein bound to ligand: [PLP].
-
-    This version formulated to work with mpmath for arbitrary floating point
-    precision and numpy arrays.
     '''
-    numerator = alpha * l_total * numpy.power(p, mpf('2'))
-    denominator = numpy.power(kd, mpf('2')) + mpf('2') * kd * p + alpha * numpy.power(p, mpf('2'))
+    numerator = alpha * l_total * numpy.power(p, 2.0)
+    denominator = numpy.power(kd, 2.0) + 2.0 * kd * p + alpha * numpy.power(p, 2.0)
     return numerator / denominator
 
 def model_func(kd, alpha, p_total, l_total):
@@ -161,59 +142,74 @@ def model_func(kd, alpha, p_total, l_total):
     q, r = calc_qr(a, b, c)
     p = []
     for a_val, q_val, r_val in zip(a, q, r):
-        if numpy.power(q_val, mpf('3')) + numpy.power(r_val, mpf('2')) > mpf('0'):
+        if numpy.power(q_val, 3.0) + numpy.power(r_val, 2.0) > 0:
             p.append(cartesian_cubic(a_val, q_val, r_val))
         else:
             p.append(polar_cubic(a_val, q_val, r_val))
     p = numpy.array(p)
     pl = get_pl(kd, alpha, l_total, p)
     plp = get_plp(kd, alpha, l_total, p)
+    print(p)
     return p, pl, plp
 
+
+def model_fitting(l_total, kd, alpha, p_total):
+    a, b, c = calc_abc(kd, alpha, p_total, l_total)
+    q, r = calc_qr(a, b, c)
+    p = []
+    for a_val, q_val, r_val in zip(a, q, r):
+        if numpy.power(q_val, 3.0) + numpy.power(r_val, 2.0) > 0:
+            p.append(cartesian_cubic(a_val, q_val, r_val))
+        else:
+            p.append(polar_cubic(a_val, q_val, r_val))
+    p = numpy.array(p)
+    plp = get_plp(kd, alpha, l_total, p)
+    return numpy.nan_to_num(plp)
 
 def fit():
     #Read in the information from the input file
     parameters, data = parse_input_file(arguments['<input>'])
     #Unpack the data list into the components
     total_ligand, y_obs, y_err, weight = data
+    print(total_ligand.dtype)
 
     #Unless working with units of concentration in the Y-observed (not my
     #present use-case) the Y-observed must be scaled and normalized in a way
     #that allows the model's predicted Y values to be relevant. This
     #consequently adds another dimension for fitting.
 
+    #Let's pick some hypothetical values for an M8 experiment, units are in uM
+    #kd = 2.5
+    #alpha = 1000
+    #p_total = 0.1
+
     if arguments['--lsq']:
-        from scipy.optimize import curve_fit
         #First argument must be the independent argument, the others will be
         #fitted
-        p_total = mpf('0.1')
-
-        def func(l_total, kd, alpha, scaling):
-            kd = mpf(kd)
-            alpha = mpf(alpha)
-            scaling = mpf(scaling)
-            p, pl, plp = model_func(kd, alpha, p_total, total_ligand)
-            return scaling * plp
-        popt, pcov = curve_fit(f=func, xdata=total_ligand, ydata=y_obs)
+        #p_total = 0.1
+        y_obs = y_obs / y_obs.max()
+        popt, pcov = curve_fit(lambda total_ligand, kd, alpha: model_fitting(total_ligand, kd, alpha, 0.1), total_ligand, y_obs, p0=(800, 100))
         print(popt)
 
+    plt.plot(total_ligand, y_obs, 'x', label='rawdata')
+    newx = numpy.logspace(0,5.1)
+    plt.plot(newx, model_fitting(newx, popt[0], popt[1], 0.1), label='fit')
+    plt.legend(loc='center right')
+    plt.xscale('log')
+    plt.grid()
+    plt.show()
 
-
-    #kd = mpf('0.02')
-    #alpha = mpf('0.05')
-    #p_total = mpf('0.1')
-    #ligand_range = mpf('0.00001') * numpy.power(mpf('10'), numpy.linspace(mpf('1'), mpf('8'), mpf('71')))
-
+    #kd = .02
+    #alpha = .05
+    #p_total = 0.1
+    #ligand_range = 0.00001 * numpy.power(10.0, numpy.linspace(1, 8, 71))
     #p, pl, plp = model_func(kd, alpha, p_total, ligand_range)
-
-
     #plt.plot(ligand_range, p / p_total, label='[P]')
     #plt.plot(ligand_range, pl / p_total, label='[PL]')
     #plt.plot(ligand_range, plp / p_total, label='[PLP]')
     #plt.legend(loc='center right')
     #plt.xscale('log')
     #plt.grid()
-
     #plt.show()
 
 
---
+++ resources/solution_space.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+The code has the straightforward aim of producing figures to illustrate the
+kinetic model for protein homo-dimerization around a bifunctional ligand. It
+will generate a 3D plot of the solution space in normalized units.
+"""
+
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+import numpy
+import matplotlib.pyplot as plt
+from matplotlib.ticker import LogLocator, FormatStrFormatter
+
+
+def calc_abc(kd, alpha, p_total, l_total):
+    '''
+    Calculates the cubic polynomial constants a, b, and c from the given Kd,
+    alpha, p_total, and l_total values.
+    '''
+    a = (2.0 * kd) / alpha + (2.0 * l_total) - p_total
+    b = (kd + 2.0 * l_total - 2.0 * p_total) * (kd / alpha)
+    c = (-1.0 * numpy.power(kd, 2.0) * p_total) / alpha
+    return a, b, c
+
+
+def calc_qr(a, b, c):
+    '''
+    Calculates Q and R (functions of a, b, and c) for the cubic solution. The
+    value of Q^3 + R^2 will determine if the solution needs to be calculated in
+    cartesian or polar coordinates.
+    '''
+    q = (3.0 * b - numpy.power(a, 2.0)) / 9.0
+    r = (9.0 * a * b - 27.0 * c - 2.0 * numpy.power(a, 3.0)) / 54.0
+    return q, r
+
+
+def cartesian_cubic(a, q, r):
+    '''
+    The cartesian coordinate solution of the cubic function; for use when
+    Q^3+R^2 > 0
+    '''
+    #Calculate the three terms of the cubic
+    first = a / -3.0
+    second = numpy.power(r + numpy.sqrt(numpy.power(q, 3) + numpy.power(r, 2)), 1.0 / 3.0)
+    third = numpy.power(r - numpy.sqrt(numpy.power(q, 3) + numpy.power(r, 2)), 1.0 / 3.0)
+    return first + second + third
+
+
+def polar_cubic(a, q, r):
+    '''
+    The polar coordinate solution of the cubic function; for use when
+    Q^3+R^2 < 0
+    '''
+    theta = numpy.arccos(r / numpy.sqrt(-1.0 * numpy.power(q, 3.0)))
+    val = numpy.cos(theta / 3.0) * numpy.sqrt(-1.0 * q) * 2.0 - a / 3.0
+    return val
+
+
+def get_pl(kd, alpha, l_total, p):
+    '''
+    After solving for [P]-free, this function will return the concentration of
+    monomer protein bound to ligand: [PL].
+    '''
+    numerator = 2.0 * kd * l_total * p
+    denominator = numpy.power(kd, 2.0) + 2.0 * kd * p + alpha * numpy.power(p, 2.0)
+    return numerator / denominator
+
+
+def get_plp(kd, alpha, l_total, p):
+    '''
+    After solving for [P]-free, this function will return the concentration of
+    dimer protein bound to ligand: [PLP].
+    '''
+    numerator = alpha * l_total * numpy.power(p, 2.0)
+    denominator = numpy.power(kd, 2.0) + 2.0 * kd * p + alpha * numpy.power(p, 2.0)
+    return numerator / denominator
+
+def model_func(kd, alpha, p_total, l_total):
+    a, b, c = calc_abc(kd, alpha, p_total, l_total)
+    q, r = calc_qr(a, b, c)
+    p = []
+    for a_val, q_val, r_val in zip(a, q, r):
+        if numpy.power(q_val, 3.0) + numpy.power(r_val, 2.0) > 0:
+            p.append(cartesian_cubic(a_val, q_val, r_val))
+        else:
+            p.append(polar_cubic(a_val, q_val, r_val))
+    p = numpy.array(p)
+    pl = get_pl(kd, alpha, l_total, p)
+    plp = get_plp(kd, alpha, l_total, p)
+    #All we care about here is PLP
+    return pl
+
+def max_plp(kd, p_total):
+    '''
+    Returns the ligand concentration at which the maximum concentration of PLP
+    will be achieved for a given Kd and P_total.
+    '''
+    return (kd + p_total) / 2.0
+
+#The following values are set for modelling a normalized system
+p_total = 1.0
+kd = 1.0
+
+#The following values are our independent variables
+l_total = numpy.logspace(-2, 3.5, 100)
+alpha = numpy.logspace(-2, 4, 100)
+
+#I considered using meshgrid here, but was confounded by the "polar or cubic"
+#operation, so I went for a bit less elegance
+plp_concentration = []
+for alpha_value in alpha:
+    plp = model_func(kd, alpha_value, p_total, l_total)
+    plp_concentration.append(plp)
+plp_soln = numpy.array(plp_concentration)
+
+
+lm, am = numpy.meshgrid(l_total, alpha)
+lm = numpy.log10(lm)
+am = numpy.log10(am)
+
+#Create the figure
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+surf = ax.plot_surface(lm, am, plp_soln, rstride=1, cstride=1,
+                       cmap=cm.coolwarm, linewidth=0, antialiased=False, alpha=0.6)
+#cset = ax.contour(lm, am, plp_soln, zdir='z', offset=0, cmap=cm.winter)
+cset = ax.contour(lm, am, plp_soln, zdir='x', offset=-2, cmap=cm.winter)
+cset = ax.contour(lm, am, plp_soln, zdir='y', offset=4, cmap=cm.winter)
+fig.colorbar(surf, shrink=0.5, aspect=5)
+ax.set_xlabel('Log-Total Ligand [L] (conc.)')
+ax.set_ylabel('Log-Cooperativity')
+ax.set_zlabel('Dimerized Protein [PLP](conc.)')
+
+
+
+plt.show()
